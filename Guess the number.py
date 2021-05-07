ae5f33fca12eaa451fd2032c11ef6be99ad67fbd
Pranavi-Duvva <78412203+Pranavi-Duvva@users.noreply.github.com>
2021-05-07 21:46:10 +0530

    Update Guess the number.py
    This is a game wherein a computer randomly generates a secret number and the user has to guess the number.

--- Guess the number.py
+++ Guess the number.py
@@ -1,13 +1,15 @@
 import random
 
 def main():
-    # secret number between 1,99
+    # Computer randomly generates a secret number between 1,99
     secret_number=int(random.randint(1,99))
-    guess=int(input("Guess the number "))
+    guess=int(input("Guess the number: "))
 
     while guess!=secret_number:
+        
         if guess<secret_number:
             print("The guess is too low!")
+            
         elif guess>secret_number:
             print("guess is too high")
         guess=int(input("Guess the number again"))
